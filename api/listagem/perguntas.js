63708339f0a389ed75eeb0d18d3a5f6186fbbc3c
Juliano Henrique Timm <120388855+JulianoTimmHub@users.noreply.github.com>
2023-03-24 22:18:02 -0300

    Enviando novas modificações do front
    Enviando novas modificações do front e realizando adaptação

---
+++ api/listagem/perguntas.js
@@ -0,0 +1,35 @@
+async function Requisicao() {
+
+    var opcoes = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        mode: "cors"
+    };
+
+    var resposta = await new Promise(function (resolver) {
+        fetch('http://localhost:8080/listagem/perguntas', opcoes).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                resolver({ sucesso: false });
+            }
+        }).then(function (data) {
+            resolver(data);
+        });
+    });
+
+    return resposta;
+
+}
+
+const Filtros = {
+    colunas: [
+        { nome: 'id', etiqueta: 'ID', tipo: 'number' },
+        {nome: 'tipo', etiqueta: 'Tipo_Pergunta', tipo: 'String'},
+        { nome: 'nome', etiqueta: 'Nome', tipo: 'string' },
+    ]
+};
+
+export { Filtros, Requisicao }
