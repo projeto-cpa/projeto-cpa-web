12f654eb33ba6ae61037a7becd0bf7a7bb567c81
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2023-03-29 20:17:39 -0300

    algumas verifications

--- components/view/cadastro/telaCadastroRespostas.vue
+++ components/view/cadastro/telaCadastroRespostas.vue
@@ -36,13 +36,7 @@ export default {
                     valido: null,
                     id: 'a' + uuidv4(),
                     tipo: 'select',
-                    valores: [
-                        {
-                            nome: this.resposta,
-                            id: 'a' + uuidv4(),
-                            valor: "true"
-                        },
-                    ],
+                    valores: [],
                     ajuda: 'Selecione uma das opções',
                     classe: {
                         coluna: 'col-12 mb-4'
@@ -94,9 +88,15 @@ export default {
                 this.validado = false
             }
         },
-        pegaTipo: function (valor) {
-            console.log("vou pegar", typeof valor);
-            this.tipoPergunta = valor;
+        pegaTipo: function (campo) {
+            console.log("campo", campo);
+            console.log("valor", campo.valor);
+            var valor = this.buscarValorEmValoresPeloId(this.formulario[this.buscarIndexPeloNome('perguntas')].valores, campo.valor);
+            console.log('valor como objeto', valor);
+            this.tipoPergunta = valor.tipo;
+            // roda a roda
+            //puxou as peguntas, porem ainda n valida os campos. quer ver dps q o professor acabar? ai a gente 
+
         },
         //e aqui esta fazendo o que?
         buscarIndexPeloNome: function (nome) {
@@ -112,6 +112,15 @@ export default {
             });
             // retorna a posicao encontrada
             return i;
+        },
+        buscarValorEmValoresPeloId(valores, id) {
+            var valor;
+            valores.forEach(function (item, index) {
+                if (item.id === id) {
+                    valor = item;
+                }
+            });
+            return valor;
         },
         receberDados: async function () {
             var that = this;
@@ -205,71 +214,64 @@ export default {
     }
 };
 </script>
-
 <template>
-    <div class="container-fluid conteudo-principal">
-        <section>
-            <article>
-                <form class="row m-0" ref="formularioCadastro">
-                    <div class="card p-0">
-                        <div class="card-body">
+                    <div class="container-fluid conteudo-principal">
+                        <section>
+                            <article>
+                                <form class="row m-0" ref="formularioCadastro">
+                                    <div class="card p-0">
+                                        <div class="card-body">
 
-                            <div v-for="campo in formulario" :key="campo.id" :class="campo.classe.coluna">
-                                <div class="form-floating">
-                                                                        <template v-if="campo.tipo !== 'textarea' && campo.tipo !== 'select'">
-                                                                            <input :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
-                                                                                :type="campo.tipo" class="form-control" :id="campo.id"
-                                                                                @keypress="campo.validar()" :class="inputClass(campo.valido)">
-                                                                        </template>
-                                                                        <template v-else-if="campo.tipo === 'select'">
-                                                                            <select :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
-                                                                                class="form-control" :id="campo.id"
-                                                                                @change="campo.validar(); pegaTipo(campo.valor);"
-                                                                                :class="inputClass(campo.valido)">
-                                                                                <option value="" disabled selected>Selecione uma Pergunta</option>
-                                                                                <option v-for="valor in campo.valores" :v-model="valor.id" :value="valor.id"
-                                                                                    :key="valor.id">
-                                                                                    {{ valor.nome }}
-                                                                                </option>
-                                                                            </select>
-                                                                        </template>
-                                                                        <template v-else>
-                                                                            <textarea :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
-                                                                                class="form-control" :id="campo.id" @keypress="campo.validar()"
-                                                                                :class="inputClass(campo.valido)"></textarea>
-                                                                        </template>
-                                                                        <label :for="campo.id" class="form-label w-100">{{ campo.etiqueta }}</label>
-                                                                        <span class="label-icon float-end" data-bs-toggle="tooltip" :data-bs-title="campo.ajuda"
-                                                                            data-bs-placement="bottom" data-bs-delay="250"><i class="fa fa-question-circle"
-                                                                                aria-hidden="true"></i></span>
-                                                                        <div class="valid-feedback">{{ campo.validacao.valido }}</div>
-                                                                        <div class="invalid-feedback">{{ campo.validacao.invalido }}</div>
-                                                                    </div>
+                                            <div v-for="campo in formulario" :key="campo.id" :class="campo.classe.coluna">
+                                                <div class="form-floating">
+                                                                    <template v-if="campo.tipo !== 'textarea' && campo.tipo !== 'select'">
+                                                                        <input :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
+                                                                            :type="campo.tipo" class="form-control" :id="campo.id"
+                                                                            @keypress="campo.validar()" :class="inputClass(campo.valido)">
+                                                                    </template>
+                                                                    <template v-else-if="campo.tipo === 'select'">
+                                                                        <select :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
+                                                                            class="form-control" :id="campo.id"
+                                                                            @change="campo.validar(); pegaTipo(campo);"
+                                                                            :class="inputClass(campo.valido)">
+                                                                            <option value="" disabled selected>Selecione uma Pergunta</option>
+                                                                            <option v-for="valor in campo.valores" :v-model="valor.id" :value="valor.id"
+                                                                                :key="valor.id">
+                                                                                {{ valor.nome }}
+                                                                            </option>
+                                                                        </select>
+                                                                    </template>
+                                                                    <template v-else>
+                                                                        <textarea :placeholder="campo.etiqueta" :name="campo.nome" v-model="campo.valor"
+                                                                            class="form-control" :id="campo.id" @keypress="campo.validar()"
+                                                                            :class="inputClass(campo.valido)"></textarea>
+                                                                    </template>
+                                                                    <label :for="campo.id" class="form-label w-100">{{ campo.etiqueta }}</label>
+                                                                    <span class="label-icon float-end" data-bs-toggle="tooltip" :data-bs-title="campo.ajuda"
+                                                                        data-bs-placement="bottom" data-bs-delay="250"><i class="fa fa-question-circle"
+                                                                            aria-hidden="true"></i></span>
+                                                                    <div class="valid-feedback">{{ campo.validacao.valido }}</div>
+                                                                    <div class="invalid-feedback">{{ campo.validacao.invalido }}</div>
                                                                 </div>
-
-                                                                <template v-if="tipoPergunta === 1">
-                                                                    <div v-for="valor in resposta" class="form-control">
+                                                            </div>
+                                                            <template v-if="tipoPergunta === 'descritiva'">
+                                                                <div v-for="valor in resposta" class="form-control">
                                                                     <textarea name="resposta" @keyup="validarRespostaDescritiva" :v-model="resposta"
                                                                         class="form-control w-100" placeholder="Preencha com a resposta...." cols="4"
                                                                         rows="4"></textarea>
-                                                                        </div>
-                                                                    <template v-if="validado !== null">
-                                                                        <div v-if="validado" class="valid-feedback">Campo validado com sucesso
-                                                                        </div>
-                                                                        <div v-else class="invalid-feedback">Campo ínvalido, tente novamente</div>
-                                                                    </template>
-                                                            
-                                                                </template>
-
-                                                                <template v-else-if="tipoPergunta === 2">
-                                                                    <div v-for="valor in valoresDaPergunta" class="form-check">
-                                                                        <input class="form-check-input" type="radio" name="opcoes" :v-model="valor.valor">
-                                                                        <label class="form-check-label" for="flexRadioDefault1">{{ valor.nome }}</label>
+                                                                </div>
+                                                                <template v-if="validado !== null">
+                                                                    <div v-if="validado" class="valid-feedback">Campo validado com sucesso
                                                                     </div>
+                                                                    <div v-else class="invalid-feedback">Campo ínvalido, tente novamente</div>
                                                                 </template>
-
-
-
+                                                            </template>
+                                                            <template v-else-if="tipoPergunta === 'objetiva'">
+                                                                <div v-for="valor in valoresDaPergunta" class="form-check">
+                                                                    <input class="form-check-input" type="radio" name="opcoes" :v-model="valor.valor">
+                                                                    <label class="form-check-label" for="flexRadioDefault1">{{ valor.nome }}</label>
+                                                                </div>
+                                                            </template>
                         </div>
                     </div>
                 </form>
