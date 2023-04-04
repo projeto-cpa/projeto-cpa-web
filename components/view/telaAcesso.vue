27f0f01728845f9411d15dae423355ba5d877621
Juliano Henrique Timm <120388855+JulianoTimmHub@users.noreply.github.com>
2023-04-04 18:22:49 -0300

    recuperando mudanças
    recuperando mudanças importantes que havia perdido

--- components/view/telaAcesso.vue
+++ components/view/telaAcesso.vue
@@ -1,3 +1,44 @@
+<script>
+export default {
+    data: function () {
+        return {
+            false: false,
+            toast: null,
+            logado: false,
+            mensagem: '',
+            validaSenha: '',
+            dados: {
+                senha: '',
+                email: ''
+            },
+            login: {
+                meuEmail: 'admim',
+                minhaSenha: 'admim'
+            },
+        }
+    },
+    methods: {
+        abrirToast: function () {
+            if (this.dados.meuEmail == '' || this.login.minhaSenha == '') {
+                this.mensagem = 'Os campos de senha e e-mail não pode ser vazios';
+            } else {
+                this.logado = true;
+                this.redirecionarParaHome()
+            }
+            this.toast.show();
+        },
+        redirecionarParaHome() {
+            this.$router.push({ path: '/' })
+        }
+    },
+    mounted: function () {
+        const bootstrap = require('bootstrap')
+        this.toast = new bootstrap.Toast(this.$refs.toast);
+    }
+}
+</script>
+
+
 <template>
     <section class="page-login">
 
@@ -10,19 +51,38 @@
                 <form class="config-form form col-12 col-md-12 col-lg-12">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="config-label-login form-label">Email:</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email" v-model="email" class="form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="config-label-login form-label">Senha:</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" v-model="senha" class="form-control" id="exampleInputPassword1">
                     </div>
                     <div class="div-esqueci-senha">
-                        <a href="/" class="btn btn-primary">Entrar</a>
+                        <button type="submit" href="" class="btn btn-primary" @click="abrirToast">Entrar</button>
                         <a class="esqueci-senha" href="/recuperar"> Esqueceu a senha ?</a>
                     </div>
                 </form>
             </div>
         </div>
+
+        <div class="toast-container position-fixed top-0 end-0 p-3">
+            <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true" ref="toast">
+                <div class="toast-header">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    <strong class="me-auto"> Atenção!</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div id="mensagem" class="toast-body">
+                    <p v-if="mensagem">{{ mensagem }}</p>
+                </div>
+            </div>
+        </div>
+
     </section>
 </template>
 
@@ -81,7 +141,8 @@
     align-items: center;
 }
 
-div,a {
+div,
+a {
     font-family: sans-serif;
 }
 </style>
