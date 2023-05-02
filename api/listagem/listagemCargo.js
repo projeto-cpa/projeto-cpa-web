b29dc0ecf325b4fda712f7b9b14c585abeca84e0
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2023-05-02 10:01:34 -0300

    Update listagemCargo.js

--- api/listagem/listagemCargo.js
+++ api/listagem/listagemCargo.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 /**
  * Retorna a listagem do cargo
  * @param {Number} pagina 
@@ -6,11 +8,14 @@
  */
 async function listagemCargo(pagina, quantidade) {
 
-    if (!pagina) {
+    pagina = Number(pagina);
+    quantidade = Number(quantidade);
+
+    if (!pagina && pagina >= 0) {
         pagina = 0;
     }
 
-    if (!quantidade) {
+    if (!quantidade && quantidade > 0) {
         quantidade = 5;
     }
 
@@ -19,23 +24,61 @@ async function listagemCargo(pagina, quantidade) {
         headers: {
             'Content-Type': 'application/json'
         },
-        mode: 'cors'
+        mode: 'cors',
+        timeout: 10000
     };
 
-    var resposta = await new Promise(function (resolver) {
-        fetch(`http://localhost:8080/cargo?pagina=${pagina}&qtd=${quantidade}`, opcoes).then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                resolver({ sucesso: false });
-            }
-        }).then(function (data) {
-            resolver(data);
+
+    try {
+        var resposta = await new Promise(function (resolver) {
+            var abort = new AbortController();
+            
+            var timeout = setTimeout(function () {
+                abort.abort();
+            }, opcoes.timeout);
+
+            fetch(
+                `http://localhost:8080/cargo?pagina=${pagina}&qtd=${quantidade}`,
+                {
+                    ...opcoes,
+                    signal: abort.signal
+                }
+            ).then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Error while getting data from API');
+                }
+            }).then(function (data) {
+                resolver({
+                    ...data,
+                    response: true
+                });
+            }).catch(function () {
+                var title = abort.signal.aborted ? 'Conexão lenta' : 'Servidor fora do ar';
+                var footer = abort.signal.aborted ? 'Tente novamente mais tarde' : 'Desculpe pelo transtorno';
+
+                Swal.fire({
+                    title: title,
+                    /*html*/
+                    html: `<div class="text-center overflow-hidden"><i class="text-primary fa fa-spinner fa-spin fa-3x fa-fw"></i></div>`,
+                    footer: 'Tente novamente mais tarde',
+                    confirmButtonText: 'Recarregar página'
+                }).then(function () {
+                    window.location.href = window.location.href;
+                })
+
+            }).finally(function () {
+                clearTimeout(timeout);
+            });
         });
-    });
-
-    return resposta;
-
+        console.log('resp', resposta)
+        return resposta;
+    } catch (e) {
+        console.log('err');
+        console.error(e);
+        throw new Error()
+    }
 }
 
 export default listagemCargo;
