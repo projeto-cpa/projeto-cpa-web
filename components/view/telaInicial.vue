95ecdcc4e449ae7bc2b1f974137e9dacd741fc1b
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-08 23:57:31 -0300

    Mais adições dashboard
    Mais adições para os blocos iniciais do dasboard

--- components/view/telaInicial.vue
+++ components/view/telaInicial.vue
@@ -1,53 +1,55 @@
 <template>
-    <div class="container-fluid conteudo-principal">
+    <div class="container-fluid">
         <section>
             <article>
                 <div class="row m-0">
                     <div class="card p-0 card-register">
-                        <div class="card-body d-flex p-3">
-                            <div class="col-12 col-lg-4 card me-3 mt-2">
+                        <div class="card-body d-flex flex-wrap justify-content-between p-3">
+                            <div class="col-12 col-lg-4 card me-3 mt-2" style="height: max-content;">
                                 <div class="div-melhor-turma">
-                                    <h5 class="subtitulo-melhor-turma m-0 p-1">Melhor turma</h5>
-                                    <div class="d-flex-wrap div-turma m-2">
-                                        <p class="text-secondary mb-2" style="font-size: 20px;">Análise e Desenvolvimento de Sistemas</p>
+                                    <h5 class="subtitulo-melhor-turma m-0 p-1">Melhor curso <i class="fa fa-graduation-cap"
+                                            aria-hidden="true"></i></h5>
+                                    <div class="div-turma m-2">
+                                        <p class="text-secondary mb-2" style="font-size: 20px;">Análise e Desenvolvimento de
+                                            Sistemas</p>
                                         <div class="div-media-turma">
                                             <p class="text-secondary mb-2 me-2">Média:
                                             <p class="mb-2" style="font-size: 30px;">8,90</p>
                                             </p>
                                         </div>
-                                        <div class="div-media-outras-turmas">
-                                            <p class="text-secondary">Média de outras turmas</p>
-                                            <div style="display: -webkit-inline-box;">
-                                                <p class="text-secondary m-0 me-2">Farmácia:
-                                                <p class="m-0"><strong>7,74</strong></p>
-                                                </p>
-                                            </div>
-                                            <div style="display: -webkit-inline-box;">
-                                                <p class="text-secondary m-0 me-2">Administração:
-                                                <p class="m-0"><strong>7,50</strong></p>
-                                                </p>
-                                            </div>
-                                            <div style="display: -webkit-inline-box;">
-                                                <p class="text-secondary m-0 me-2">Ciência de dados:
-                                                <p class="m-0"><strong>7,28</strong></p>
-                                                </p>
-                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-4 card me-3 mt-2" style="height: max-content;">
+                                <div class="div-pior-turma">
+                                    <h5 class="subtitulo-pior-turma m-0 p-1">Pior curso <i class="fa fa-graduation-cap"
+                                            aria-hidden="true"></i></h5>
+                                    <div class="div-turma m-2">
+                                        <p class="text-secondary mb-2" style="font-size: 20px;">Engenharia da computação</p>
+                                        <div class="div-media-turma">
+                                            <p class="text-secondary mb-2 me-2">Média:
+                                            <p class="mb-2" style="font-size: 30px;">6,87</p>
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-4 card me-3 mt-2">
-                                <div>
-                                    <h5 class="text-secondary">Pior turma</h5>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-3 card mt-2">
-                                <div>
-                                    <h5 class="text-secondary">Principais feedbacks</h5>
+                            <div class="col-12 col-lg-3 card me-3 mt-2" style="height: max-content;">
+                                <div class="div-principais-feedbacks">
+                                    <h5 class="subtitulo-principais-feedbacks m-0 p-1">Principais feedbacks <i class="fa fa-comments"
+                                            aria-hidden="true"></i></h5>
+                                    <div class="div-feedbacks m-2">
+                                        <p class="text-secondary mb-2" style="font-size: 20px;">Engenharia da computação</p>
+                                        <div class="div-media-turma">
+                                            <p class="text-secondary mb-2 me-2">Média:
+                                            <p class="mb-2" style="font-size: 30px;">6,87</p>
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body d-flex">
                             <div class="col-12 card">
                                 <div>
                                     <h5 class="text-secondary">Métricas</h5>
@@ -65,11 +67,24 @@
 
 <style scoped>
 .div-melhor-turma {
-    background-color: aliceblue;
+    border-radius: 5px;
+}
+
+.div-pior-turma {
+    border-radius: 5px;
+}
+
+.div-principais-feedbacks {
     border-radius: 5px;
 }
 
 .div-turma {
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+}
+
+.div-feedbacks {
     justify-content: center;
     align-items: center;
     font-weight: 500;
@@ -91,6 +106,22 @@
 
 .subtitulo-melhor-turma {
     background-color: #68d36b;
+    text-align: center;
+    color: #fff !important;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+}
+
+.subtitulo-pior-turma {
+    background-color: #d46a5c;
+    text-align: center;
+    color: #fff !important;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+}
+
+.subtitulo-principais-feedbacks {
+    background-color: #0d6efd;
     text-align: center;
     color: #fff !important;
     border-top-right-radius: 5px;
