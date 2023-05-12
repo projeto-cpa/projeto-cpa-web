9c126dc423a2b6e610fe3ee07514011418d0961b
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-12 00:09:43 -0300

    Estruturando novamente a tela de login

--- components/view/telaInicial.vue
+++ components/view/telaInicial.vue
@@ -1,60 +1,151 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid conteudo-principal">
         <section>
             <article>
-                <div class="row m-0">
-                    <div class="card p-0 card-register">
-                        <div class="card-body d-flex flex-wrap justify-content-between p-3">
-                            <div class="col-12 col-lg-4 card me-3 mt-2">
-                                <div class="div-melhor-turma">
-                                    <h5 class="subtitulo-melhor-turma m-0 p-1">Melhor curso <i class="fa fa-graduation-cap"
-                                            aria-hidden="true"></i></h5>
-                                    <div class="div-turma m-3">
-                                        <div>
-                                            <p class="text-secondary mb-2" style="font-size: 20px;">Análise e
-                                                Desenvolvimento de
-                                                Sistemas</p>
+                <div class="card p-0 card-register">
+                    <div class="row m-0">
+
+                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Earnings (Monthly)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
                                         </div>
-                                        <div class="div-media-turma">
-                                            <p class="text-secondary mb-2 me-2">Média:
-                                            <p class="mb-2" style="font-size: 30px;">8,90</p>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-4 card me-3 mt-2" style="height: max-content;">
-                                <div class="div-pior-turma">
-                                    <h5 class="subtitulo-pior-turma m-0 p-1">Pior curso <i class="fa fa-graduation-cap"
-                                            aria-hidden="true"></i></h5>
-                                    <div class="div-turma m-3">
-                                        <p class="text-secondary mb-2" style="font-size: 20px;">Engenharia da computação</p>
-                                        <div class="div-media-turma">
-                                            <p class="text-secondary mb-2 me-2">Média:
-                                            <p class="mb-2" style="font-size: 30px;">6,87</p>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-lg-3 card me-3 mt-2" style="height: max-content;">
-                                <div class="div-principais-feedbacks">
-                                    <h5 class="subtitulo-principais-feedbacks m-0 p-1">Principais feedbacks <i
-                                            class="fa fa-comments" aria-hidden="true"></i></h5>
-                                    <div class="div-feedbacks m-2">
-                                        <div>
-                                            <p class="text-secondary mb-2" style="font-size: 20px;"></p>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-body d-flex">
-                            <div class="col-12 card">
-                                <div style="border-radius: 5px;">
-                                    <h5 class="subtitulo-metricas">Métricas</h5>
-                                    <div>
-                                        <img src="../../static/foto-métricas.png" style="width: -webkit-fill-available;">
+
+                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Earnings (Annual)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar"
+                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                                            aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Pending Requests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row m-0">
+                        <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                            aria-labelledby="dropdownMenuLink">
+                                            <div class="dropdown-header">Dropdown Header:</div>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                        <canvas id="myAreaChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                            aria-labelledby="dropdownMenuLink">
+                                            <div class="dropdown-header">Dropdown Header:</div>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-pie pt-4 pb-2">
+                                        <canvas id="myPieChart"></canvas>
+                                    </div>
+                                    <div class="mt-4 text-center small">
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-primary"></i> Direct
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-success"></i> Social
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-info"></i> Referral
+                                        </span>
                                     </div>
                                 </div>
                             </div>
@@ -63,80 +154,14 @@
                 </div>
             </article>
         </section>
-        <footer class="form-footer bg-white">
-        </footer>
     </div>
 </template>
 
+
+
 <style scoped>
-.div-melhor-turma {
-    height: 200px;
-}
-
-.div-pior-turma {
-    height: 200px;
-}
-
-.div-principais-feedbacks {}
-
-.div-turma {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: space-between;
-    height: 130px;
-    font-weight: 500;
-}
-
-.div-feedbacks {
-    justify-content: center;
-    align-items: center;
-    font-weight: 500;
-}
-
-.div-media-turma {
-    display: inline-flex;
-    align-items: center;
-}
-
-.div-media-outras-turmas {
-    display: flex;
-    flex-direction: column;
-}
-
-.text-secondary {
-    --bs-text-opacity: 1;
-}
-
-.subtitulo-melhor-turma {
-    background-color: #68d36b;
-    text-align: center;
-    color: #fff !important;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-}
-
-.subtitulo-pior-turma {
-    background-color: #d46a5c;
-    text-align: center;
-    color: #fff !important;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-}
-
-.subtitulo-principais-feedbacks {
-    background-color: #0d6efd;
-    text-align: center;
-    color: #fff !important;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-}
-
-.subtitulo-metricas {
-    background-color: #c9d400;
-    text-align: center;
-    color: #fff !important;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
+.conteudo-principal {
+    padding: 0 !important;
+    overflow-y: auto !important;
 }
 </style>
