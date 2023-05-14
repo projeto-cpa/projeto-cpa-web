ff30277aa87c76169d972e3a724b582e1ef57178
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-14 18:38:08 -0300

    Tela base da dashboard pronta

--- components/view/telaInicial.vue
+++ components/view/telaInicial.vue
@@ -2,155 +2,114 @@
     <div class="container-fluid conteudo-principal">
         <section>
             <article>
-                <div class="card p-0 card-register">
+                <div class="card p-0 card-register pe-1 ps-1">
                     <div class="row m-0">
+                        <div class="col-xl-8 d-flex flex-wrap">
 
-                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                            <div class="col-12 col-xl-6 col-md-6 mb-3 mt-3 pe-2 ps-2">
+                                <div class="card shadow h-100 py-2"
+                                    style="border-left: 0.25rem solid rgba(25,135,84,1)!important;">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center justify-content-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs text-success text-uppercase mb-1"
+                                                    style="font-weight: 500!important;">
+                                                    Melhor curso</div>
+                                                <div class="h5 font-weight-bold text-gray-800 mb-3">Ciência de Dados
                                                 </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
+                                                <div class="d-flex align-items-center" style="gap: 1em;">
+                                                    <p class="text-uppercase m-0 p-0"
+                                                        style="font-weight: 500!important; font-size: small;">Média:</p>
+                                                    <p class="text-uppercase m-0 p-0"
+                                                        style="font-weight: 500!important; font-size: x-large;">8,90</p>
                                                 </div>
                                             </div>
+                                            <div class="col-auto">
+                                                <i class="fa fa-graduation-cap fa-2x text-gray-300"></i>
+                                            </div>
                                         </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-xl-6 col-md-6 mb-3 mt-3 pe-2 ps-2">
+                                <div class="card shadow h-100 py-2"
+                                    style="border-left: 0.25rem solid rgba(255,193,7,1)!important;">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center justify-content-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1"
+                                                    style="font-weight: 500!important;">
+                                                    Pior curso</div>
+                                                <div class="h5 font-weight-bold text-gray-800 mb-3">Farmácia</div>
+                                                <div class="d-flex align-items-center" style="gap: 1em;">
+                                                    <p class="text-uppercase m-0 p-0"
+                                                        style="font-weight: 500!important; font-size: small;">Média:</p>
+                                                    <p class="text-uppercase m-0 p-0"
+                                                        style="font-weight: 500!important; font-size: x-large;">6,75</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fa fa-graduation-cap fa-2x text-gray-300"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-12 pe-2 ps-2">
+                                <div class="card shadow mb-4">
+                                    <div
+                                        class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                        <h6 class="font-weight-bold text-primary text-uppercase m-0">Métricas</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="chart-area">
+                                            <img class="img-metricas" src="../../static/foto-métricas.png">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-xl-3 col-md-6 mb-3 mt-3">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                        <div class="col-xl-4 pe-2 ps-2">
+                            <div class="col-xl-12  mb-3 mt-3">
+                                <div class="card shadow mb-4">
+                                    <div
+                                        class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                        <h6 class="m-0 font-weight-bold text-primary text-uppercase mb-0">Principais
+                                            feedbacks
+                                        </h6>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="col-xl-12 mb-2" style="border-bottom: 1px solid #000;">
+                                            <p class="mb-0" style="font-weight: 500!important;">Important Appointment</p>
+                                            <p class="mb-2">9:30 AM - 9:45 AM</p>
                                         </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        <div class="col-xl-12 mb-2" style="border-bottom: 1px solid #000;">
+                                            <p class="mb-0" style="font-weight: 500!important;">Important Appointment</p>
+                                            <p class="mb-2">9:30 AM - 9:45 AM</p>
+                                        </div>
+                                        <div class="col-xl-12 mb-2" style="border-bottom: 1px solid #000;">
+                                            <p class="mb-0" style="font-weight: 500!important;">Important Appointment</p>
+                                            <p class="mb-2">9:30 AM - 9:45 AM</p>
+                                        </div>
+                                        <div class="col-xl-12 mb-2" style="border-bottom: 1px solid #000;">
+                                            <p class="mb-0" style="font-weight: 500!important;">Important Appointment</p>
+                                            <p class="mb-2">9:30 AM - 9:45 AM</p>
+                                        </div>
+                                        <div class="col-xl-12 mb-2" style="border-bottom: 1px solid #000;">
+                                            <p class="mb-0" style="font-weight: 500!important;">Important Appointment</p>
+                                            <p class="mb-2">9:30 AM - 9:45 AM</p>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
+
                     </div>
 
-                    <div class="row m-0">
-                        <div class="col-xl-8 col-lg-7">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <div class="chart-pie pt-4 pb-2">
-                                        <canvas id="myPieChart"></canvas>
-                                    </div>
-                                    <div class="mt-4 text-center small">
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-primary"></i> Direct
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-success"></i> Social
-                                        </span>
-                                        <span class="mr-2">
-                                            <i class="fas fa-circle text-info"></i> Referral
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </article>
         </section>
@@ -163,5 +122,10 @@
 .conteudo-principal {
     padding: 0 !important;
     overflow-y: auto !important;
+}
+
+.img-metricas {
+    max-width: 100%;
+    max-height: 100%;
 }
 </style>
