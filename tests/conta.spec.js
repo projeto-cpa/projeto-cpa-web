0eccd8ad3979cbad083f744ed420a6840ed2548e
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2023-06-30 12:37:13 -0300

    PR: Feature recuperar acesso (#41)
    * bkp
    
    * bkp
    
    * bkp
    
    * bkp
    
    * push last

--- tests/conta.spec.js
+++ tests/conta.spec.js
@@ -16,6 +16,13 @@ const senhaNova = '[type="password"][id="nova-senha"]';
 const confirmarSenha = '[type="password"][id="confirmar-senha"]';
 const clickSalvar = '[type="button"][class="btn btn-primary"]';
 const clickConfirmar = '[class="swal2-confirm swal2-styled"]';
+const inputFileFoto = '#upload-foto'
+const pathFoto = 'C:/Users/LucasdeOliveiraNeitz/OneDrive - Biopark/Área de Trabalho/logo_azul.png'
+const filenameFoto = 'logo_azul.png'
+const labelNomeFoto = '#label-foto'
+const alertSelector = '#swal2-title.swal2-title';
+const alertText = 'Confirmar alteração';
+const clickConfirmSelector = '.swal2-container .swal2-actions .swal2-confirm';
 const clickFoto = '[id="customFile"][type="file"]';
 const [fileChooser] = await Promise.all([
   // It is important to call waitForEvent before click to set up waiting.
@@ -63,30 +70,69 @@ test('alterar senha passa', async ({ page }) => {
   await page.close();
 });
 
-test('alterar foto', async ({ page }) => {
+// test('enviar foto', async ({ page }) => {
 
-  await page.goto(serverURL);
+//   await page.goto(serverURL);
 
-  await page.fill(emailSelector, email);
-  await page.fill(passwordSelector, password);
+//   await page.fill(emailSelector, email);
+//   await page.fill(passwordSelector, password);
 
-  // chec if the email is equals to the value filled
-  await expect(page.locator(emailSelector)).toHaveValue(email);
-  await expect(page.locator(passwordSelector)).toHaveValue(password);
+//   // chec if the email is equals to the value filled
+//   await expect(page.locator(emailSelector)).toHaveValue(email);
+//   await expect(page.locator(passwordSelector)).toHaveValue(password);
 
-  // click on the button
-  await page.click(submitSelector);
+//   // click on the button
+//   await page.click(submitSelector);
 
-  // verify if the user was redirected to "/" 
-  await expect(page).toHaveURL(serverURL + '/');
+//   // verify if the user was redirected to "/" 
+//   await expect(page).toHaveURL(serverURL + '/');
 
-  await page.click(clickConta);
+//   await page.click(clickConta);
 
-  await fileChooser.setFiles([
-    'D:/GitHub/teste.PNG'
-  ])
+//   await expect(page.locator("header h1")).toHaveText('Tela minha conta');
+
+//   await page.click(inputFileFoto); 
+
+//   await page.setInputFiles(inputFileFoto, pathFoto);
+
+//   await expect(page.locator(alertSelector)).toHaveText(alertText);
+
+//   await page.click(clickConfirmSelector);
   
-  await page.click(clickFoto);
+//   await expect(page.locator(labelNomeFoto)).toContainText(filenameFoto);
 
-  await page.close();
-}); 
+// });
+
+// test('alterar foto', async ({ page }) => {
+
+// const [fileChooser] = await Promise.all([
+//   // It is important to call waitForEvent before click to set up waiting.
+//   page.waitForEvent('filechooser'),
+//   // Opens the file chooser.
+//   page.locator('#uploadFile').click(),
+// ])
+
+//   await page.goto(serverURL);
+
+//   await page.fill(emailSelector, email);
+//   await page.fill(passwordSelector, password);
+
+//   // chec if the email is equals to the value filled
+//   await expect(page.locator(emailSelector)).toHaveValue(email);
+//   await expect(page.locator(passwordSelector)).toHaveValue(password);
+
+//   // click on the button
+//   await page.click(submitSelector);
+
+//   // verify if the user was redirected to "/"
+//   await expect(page).toHaveURL(serverURL + '/');
+
+//   await page.click(clickConta);
+//   await fileChooser.setFiles([
+//     'D:/GitHub/teste.PNG'
+//   ])
+//   await page.click(clickFoto);
+
+//   await page.close();
+// });
+
